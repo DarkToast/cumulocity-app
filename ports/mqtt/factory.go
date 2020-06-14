466b1a3dba0c64d8f565f28f0743d7b0622307d1
Christian Schmidt <c.schmidt@tarent.de>
2020-06-14 21:58:32 +0200

    Adding an injected configuration

--- ports/mqtt/factory.go
+++ ports/mqtt/factory.go
@@ -4,19 +4,18 @@ import (
 	"fmt"
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 	"log"
+	"tarent.de/schmidt/client-user/application"
 )
 
-var MQTT_HOSTNAME = "192.168.0.11"
-var PORT = 1883
 var port *Port
 
-func CreatePort() *Port {
+func CreatePort(configuration *application.Configuration) *Port {
 	if port != nil {
 		return port
 	}
 
 	clientOpts := MQTT.NewClientOptions().
-		AddBroker(fmt.Sprintf("tcp://%s:%d", MQTT_HOSTNAME, PORT)).
+		AddBroker(fmt.Sprintf("tcp://%s:%d", configuration.MQTT_HOSTNAME, configuration.MQTT_PORT)).
 		SetClientID("gateway").
 		SetCleanSession(true)
 	mqttClient := MQTT.NewClient(clientOpts)
